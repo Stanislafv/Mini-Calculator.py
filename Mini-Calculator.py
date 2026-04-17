b14fe410f92ba2a073f930f90204aa5397afe8a4
Stanislafv <klimov.stanislaff@mail.ru>
2026-04-17 23:58:23 +0300

    Update Mini-Calculator.py

--- Mini-Calculator.py
+++ Mini-Calculator.py
@@ -16,6 +16,5 @@ def calculator(com):
     display.config(text=text); text = text[:16] 
 names = [("7",1,1), ("8",1,2), ("9",1,3), ("4",2,1), ("5",2,2), ("6",2,3), ("1",3,1), ("2",3,2), ("3",3,3), ("0",4,2), ("C",1,0,), ("=",4,1,), ("⌫",4,3), ("+",4,4), ("-",3,4), ("/",2,4), ("*",1,4), ("**",2,0), (".",4,0), ("±",3,0), ("(",5,1), (")",5,2)]
 for (com,row,col) in names:
-    btn = tk.Button(root, text=com, width=2, height=1, font=("Cascadia Code", 24, "bold"), command=lambda r=row, c=col, com=com: calculator(com))
-    btn.grid(row=row, column=col)
+    btn = tk.Button(root, text=com, width=2, height=1, font=("Cascadia Code", 24, "bold"), command=lambda r=row, c=col, com=com: calculator(com));btn.grid(row=row, column=col)
 root.bind('<Return>', lambda fff: calculator('=')); root.mainloop()
